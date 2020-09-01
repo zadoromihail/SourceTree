0408900247a687d4c2e69dcc1da8acef8b4e45e7
Mihail <zadoromihail@gmail.com>
2020-09-01 23:40:09 +0300

    Added new functions

--- SourceTree/ViewController.swift
+++ SourceTree/ViewController.swift
@@ -12,9 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Hello world")
     }
-
+    func printer() {
+    print("something")
+    }
 
 }
 
